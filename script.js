7172cedadd90c6e3490b7a6c9933dd56d0ceb82c
Rob LeGrand <roblegrand98@gmail.com>
2025-04-29 17:23:27 -0500

    Rewrite applyStrategyA, applyStrategyB, applyStrategyT and applyStrategyW functions to be much more graceful

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
 /*jslint devel */
 /*jshint esnext: true */
-// https://jsbin.com/durayeseke/edit?js,console
-// old: https://jsbin.com/layetuneqe/edit?js,console
+// https://jsbin.com/jebiputove/edit?js,console
+// old: https://jsbin.com/durayeseke/edit?js,console
+// older: https://jsbin.com/layetuneqe/edit?js,console
 // older: https://jsbin.com/tutuhayesi/edit?js,console
 
 (function () {
    'use strict';
 
-   const firstNotEqualTo = (array, value) => (
-      array.length <= 0
-      ? Number.POSITIVE_INFINITY
-      : array[0] !== value
-      ? array[0]
-      : firstNotEqualTo(array.slice(1), value)
+   const calcAverage = (array) => array.reduce(
+      (totalSoFar, element) => totalSoFar + element,
+      0
+   ) / array.length;
+
+   const isDecisive = (array, value) => (
+      array.every(
+         (cutoff) => value >= cutoff
+      ) && array.some(
+         (cutoff) => value > cutoff
+      )
+   ) || (
+      array.every(
+         (cutoff) => value <= cutoff
+      ) && array.some(
+         (cutoff) => value < cutoff
+      )
    );
 
-   const applyStrategyA = function (cardinalPreferences, poll) {
+   const findDecisiveCutoff = (array, value) => (
+      array.length <= 0
+      ? Number.NaN
+      : (
+         Number.isFinite(array[0])
+         && array[0] !== value
+      )
+      ? array[0]
+      : (
+         Array.isArray(array[0])
+         && isDecisive(array[0], value)
+      )
+      ? calcAverage(array[0])
+      : findDecisiveCutoff(array.slice(1), value)
+   );
+
+   const decideApproval = (cardinalPreference, cutoff, defaultVote) => (
+      cardinalPreference > cutoff
+      ? 1
+      : cardinalPreference < cutoff
+      ? 0
+      : defaultVote
+   );
+
+   const applyStrategyA = function (cardinalPreferences, poll, lastBallot) {
       console.log('---------------------------');
       const candidates = poll.map(
          (ignore, index) => index
@@ -32,20 +68,21 @@
       );
       console.log('topCandidates:', topCandidates);
       const cutoffs = topCandidates.map(
-         (candidateSet) => candidateSet.reduce(
-            (totalSoFar, candidate) => totalSoFar + cardinalPreferences[candidate],
-            0
-         ) / candidateSet.length
+         (candidateSet) => calcAverage(
+            candidateSet.map(
+               (candidate) => cardinalPreferences[candidate]
+            )
+         )
       );
       console.log('cutoffs:', cutoffs);
       return candidates.map(
-         (candidate) => (
-            cardinalPreferences[candidate] > firstNotEqualTo(
+         (candidate) => decideApproval(
+            cardinalPreferences[candidate],
+            findDecisiveCutoff(
                cutoffs,
                cardinalPreferences[candidate]
-            )
-            ? 1
-            : 0
+            ),
+            lastBallot[candidate]
          )
       );
    };
@@ -60,66 +97,31 @@
          (x, y) => y - x
       );
       console.log('orderedPollTotals:', orderedPollTotals);
-      const topCandidates = candidates.filter(
-         (candidate) => poll[candidate] === orderedPollTotals[0]
+      const topCandidates = orderedPollTotals.map(
+         (pollTotal) => candidates.filter(
+            (candidate) => poll[candidate] >= pollTotal
+         )
       );
       console.log('topCandidates:', topCandidates);
-      const nextTopCandidates = candidates.filter(
-         (candidate) => poll[candidate] === orderedPollTotals[1]
-      );
-      console.log('nextTopCandidates:', nextTopCandidates);
-      const topCandPrefs = topCandidates.map(
-         (candidate) => cardinalPreferences[candidate]
-      ).toSorted(
-         (x, y) => y - x
-      );
-      console.log('topCandPrefs:', topCandPrefs);
-      const nextTopCandPrefs = nextTopCandidates.map(
-         (candidate) => cardinalPreferences[candidate]
-      ).toSorted(
-         (x, y) => y - x
-      );
-      console.log('nextTopCandPrefs:', nextTopCandPrefs);
-      const cutoffs = (
-         topCandidates.length > 1
-         ? [
-            topCandPrefs.reduce(
-               (totalSoFar, candPref, index) => totalSoFar + candPref * index,
-               0
-            ) / (topCandidates.length * (topCandidates.length - 1) / 2),
-            topCandPrefs.toReversed().reduce(
-               (totalSoFar, candPref, index) => totalSoFar + candPref * index,
-               0
-            ) / (topCandidates.length * (topCandidates.length - 1) / 2)
-         ]
-         : [
-            nextTopCandPrefs.reduce(
-               (totalSoFar, candPref) => totalSoFar + Math.min(topCandPrefs[0], candPref),
-               0
-            ) / nextTopCandidates.length,
-            nextTopCandPrefs.reduce(
-               (totalSoFar, candPref) => totalSoFar + Math.max(topCandPrefs[0], candPref),
-               0
-            ) / nextTopCandidates.length
-         ]
-      );
+      const cutoffs = [
+         topCandidates[1].map(
+            (candidate) => cardinalPreferences[candidate]
+         )
+      ];
       console.log('cutoffs:', cutoffs);
       return candidates.map(
-         (candidate) => (
-            cutoffs.every(
-               (cutoff) => cardinalPreferences[candidate] <= cutoff
-            )
-            ? 0
-            : cutoffs.every(
-               (cutoff) => cardinalPreferences[candidate] >= cutoff
-            )
-            ? 1
-            : lastBallot[candidate]
+         (candidate) => decideApproval(
+            cardinalPreferences[candidate],
+            findDecisiveCutoff(
+               cutoffs,
+               cardinalPreferences[candidate]
+            ),
+            lastBallot[candidate]
          )
       );
    };
 
-   const applyStrategyT = function (cardinalPreferences, poll) {
+   const applyStrategyT = function (cardinalPreferences, poll, lastBallot) {
       console.log('---------------------------');
       const candidates = poll.map(
          (ignore, index) => index
@@ -129,64 +131,40 @@
          (x, y) => y - x
       );
       console.log('orderedPollTotals:', orderedPollTotals);
-      const topCandidates = candidates.filter(
-         (candidate) => poll[candidate] === orderedPollTotals[0]
+      const topCandidates = orderedPollTotals.map(
+         (pollTotal) => candidates.filter(
+            (candidate) => poll[candidate] >= pollTotal
+         )
       );
       console.log('topCandidates:', topCandidates);
-      const nextTopCandidates = candidates.filter(
-         (candidate) => poll[candidate] === orderedPollTotals[1]
-      );
-      console.log('nextTopCandidates:', nextTopCandidates);
-      const topCandPrefs = topCandidates.map(
-         (candidate) => cardinalPreferences[candidate]
-      ).toSorted(
-         (x, y) => y - x
-      );
-      console.log('topCandPrefs:', topCandPrefs);
-      const nextTopCandPrefs = nextTopCandidates.map(
-         (candidate) => cardinalPreferences[candidate]
-      ).toSorted(
-         (x, y) => y - x
-      );
-      console.log('nextTopCandPrefs:', nextTopCandPrefs);
-      const cutoffs = (
-         topCandidates.length > 1
-         ? [
-            topCandPrefs.reduce(
-               (totalSoFar, candPref, index) => totalSoFar + candPref * index,
-               0
-            ) / (topCandidates.length * (topCandidates.length - 1) / 2),
-            topCandPrefs.toReversed().reduce(
-               (totalSoFar, candPref, index) => totalSoFar + candPref * index,
-               0
-            ) / (topCandidates.length * (topCandidates.length - 1) / 2)
-         ]
-         : [
-            nextTopCandPrefs.reduce(
-               (totalSoFar, candPref) => totalSoFar + Math.min(topCandPrefs[0], candPref),
-               0
-            ) / nextTopCandidates.length,
-            nextTopCandPrefs.reduce(
-               (totalSoFar, candPref) => totalSoFar + Math.max(topCandPrefs[0], candPref),
-               0
-            ) / nextTopCandidates.length
-         ]
-      );
+      const cutoffs = [
+         Math.max(
+            ...topCandidates[1].map(
+               (candidate) => cardinalPreferences[candidate]
+            )
+         ),
+         ...topCandidates.map(
+            (candidateSet) => calcAverage(
+               candidateSet.map(
+                  (candidate) => cardinalPreferences[candidate]
+               )
+            )
+         )
+      ];
       console.log('cutoffs:', cutoffs);
       return candidates.map(
-         (candidate) => (
-            (cutoffs.every(
-               (cutoff) => cardinalPreferences[candidate] >= cutoff
-            ) && !cutoffs.every(
-               (cutoff) => cardinalPreferences[candidate] <= cutoff
-            ))
-            ? 1
-            : 0
+         (candidate) => decideApproval(
+            cardinalPreferences[candidate],
+            findDecisiveCutoff(
+               cutoffs,
+               cardinalPreferences[candidate]
+            ),
+            lastBallot[candidate]
          )
       );
    };
 
-   const applyStrategyW = function (cardinalPreferences, poll) {
+   const applyStrategyW = function (cardinalPreferences, poll, lastBallot) {
       console.log('---------------------------');
       const candidates = poll.map(
          (ignore, index) => index
@@ -203,15 +181,17 @@
       );
       console.log('topCandidates:', topCandidates);
       const cutoffs = topCandidates.map(
-         (candidateSet) => candidateSet.reduce(
-            (totalSoFar, candidate) => totalSoFar + cardinalPreferences[candidate],
-            0
-         ) / candidateSet.length
+         (candidateSet) => calcAverage(
+            candidateSet.map(
+               (candidate) => cardinalPreferences[candidate]
+            )
+         )
       );
       console.log('cutoffs:', cutoffs);
       return candidates.map(
-         (candidate) => (
-            cardinalPreferences[candidate] > firstNotEqualTo(
+         (candidate) => decideApproval(
+            cardinalPreferences[candidate],
+            findDecisiveCutoff(
                cutoffs.slice(
                   topCandidates.findIndex(
                      (candidateSet) => candidateSet.includes(
@@ -220,9 +200,8 @@
                   )
                ),
                cardinalPreferences[candidate]
-            )
-            ? 1
-            : 0
+            ),
+            lastBallot[candidate]
          )
       );
    };
